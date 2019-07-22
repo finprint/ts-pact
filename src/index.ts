438fee0ac943a9561269c3ea862d062edabb7585
ken <ken@blend.com>
2019-07-22 16:43:07 -0700

    Add typed generateKeyPair

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
+import pactLang from 'pact-lang-api'
+
 import PactApi from './pactApi'
 import * as pactUtils from './pactUtils'
 import { IKeyPair } from './types'
 
+function generateKeyPair(): IKeyPair {
+  return pactLang.crypto.genKeyPair()
+}
+
 export {
   IKeyPair,
   PactApi,
+  generateKeyPair,
   pactUtils,
 }
