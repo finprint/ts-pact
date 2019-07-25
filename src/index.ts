cead09e2f6171fe46207e556599c159081445841
ken <ken@blend.com>
2019-07-24 17:17:29 -0700

    Remove generateKeyPair from index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,8 @@ import PactApi from './pactApi'
 import * as pactUtils from './pactUtils'
 import { IKeyPair } from './types'
 
-const generateKeyPair = pactUtils.generateKeyPair
-
 export {
   IKeyPair,
   PactApi,
-  generateKeyPair,
   pactUtils,
 }
